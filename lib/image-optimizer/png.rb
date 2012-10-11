d6c8ebe032798a115fdc4c07ede332f26dd4ab29
Martin Kozák <martinkozak@martinkozak.net>
2012-10-12 00:06:09 +0200

    correct using of temporary directory, alpha channel buggy converting corrected using imagemagick

--- lib/image-optimizer/png.rb
+++ lib/image-optimizer/png.rb
@@ -22,38 +22,37 @@ module ImageOptimizer
             block.call(path.dup, ImageOptimizer::BEFORE)
                       
             # New method (GIMP & XcfTools)
-            if ImageOptimizer.available? :gimp and ImageOptimizer.available? :xcf2png 
-                dirname = File.dirname(path)
-                basename = File.basename(path, '.png')
-                basepath = dirname + '/' + basename
-                png_path = basepath + '.png'
-                xcf_path = basepath + '.xcf'
-                protect = File.exists? xcf_path
+            if ImageOptimizer.available? :gimp and ImageOptimizer.available? :xcf2png and ImageOptimizer.available? :convert
                 
-                if protect
-                    protect = Dir.tmpdir + '/' + File.basename(xcf_path)
-                    FileUtils.mv(xcf_path, protect)
-                end
-              
-                # GIMP
-                block.call(:gimp, ImageOptimizer::METHOD)
-                gimp = File.read(File.dirname(__FILE__) + '/png.script')
-                gimp.gsub!(':path', png_path)
-                `#{gimp}`
+                Dir.mktmpdir do |tmpdir|
+                
+                    dirname = tmpdir
+                    basename = File.basename(path, '.png')
+                    basepath = dirname + '/' + basename 
+                    png_path = basepath + '.png'
+                    xcf_path = basepath + '.xcf'
+                    tif_path = basepath + '.tif'
+                    
+                    # ImageMagick
+                    block.call(:mogrify, ImageOptimizer::METHOD)
+                    `convert #{path_escaped} #{Shellwords::escape(tif_path)} 2> /dev/null`
+                  
+                    # GIMP
+                    block.call(:gimp, ImageOptimizer::METHOD)
+                    gimp = File.read(File.dirname(__FILE__) + '/png.script')
+                    gimp.gsub!(':path', tif_path)
+                    `#{gimp}`
 
-                # xcf2png
-                block.call(:xcf2png, ImageOptimizer::METHOD)
-                `xcf2png #{Shellwords::escape(xcf_path)} > #{Shellwords::escape(png_path)} 2> /dev/null`
-                FileUtils.rm(xcf_path)
-                
-                if protect
-                    FileUtils.mv(protect, xcf_path)
+                    # xcf2png
+                    block.call(:xcf2png, ImageOptimizer::METHOD)
+                    `xcf2png #{Shellwords::escape(xcf_path)} > #{Shellwords::escape(path_escaped)} 2> /dev/null`
+                    
                 end
-                         
+                
             # Old method (ImageMagick)
-            elsif ImageOptimizer.available? :convert
+            elsif ImageOptimizer.available? :mogrify
                 block.call(:convert, ImageOptimizer::METHOD)
-                `convert #{path_escaped} -quality 100 #{path_escaped} 2> /dev/null`
+                `mogrify #{path_escaped} -quality 100 2> /dev/null`
             end
 
             # General optimizers
